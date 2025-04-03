a145a0d11f2c208f4335342c251e493adebaa011
Kennedy <samribe@outlook.com>
2025-04-02 21:38:48 -0300

    Criando o controller

---
+++ src/main/java/dev/myproject/CadastroDeNinjas/Controller.java
@@ -0,0 +1,15 @@
+package dev.myproject.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController // lida com as requisições
+@RequestMapping // mapeia requisições
+public class Controller {
+
+    @GetMapping("/boasvindas") // cria a rota e pega informacoes
+    public String boasVindas() {
+        return "Essa é a minha primeira rota";
+    }
+}
