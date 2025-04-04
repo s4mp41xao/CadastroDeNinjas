def76d0e29309dce9abf527eb175e7b6d1f44470
Kennedy <samribe@outlook.com>
2025-04-04 14:46:26 -0300

    feat: create MissoesRepository

---
+++ src/main/java/dev/myproject/CadastroDeNinjas/Missoes/MissoesRepository.java
@@ -0,0 +1,5 @@
+package dev.myproject.CadastroDeNinjas.Missoes;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MissoesRepository extends JpaRepository<MissoesModel, Long> {
+}
