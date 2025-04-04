b7524fb9a2b82a0c7d263b942912df877e349d21
Kennedy <samribe@outlook.com>
2025-04-04 11:36:38 -0300

    feat: include lombok in the MissoesModel and NinjaModel classes for abstraction

--- src/main/java/dev/myproject/CadastroDeNinjas/Missoes/MissoesModel.java
+++ src/main/java/dev/myproject/CadastroDeNinjas/Missoes/MissoesModel.java
@@ -1,12 +1,16 @@
 package dev.myproject.CadastroDeNinjas.Missoes;
-
 import dev.myproject.CadastroDeNinjas.Ninjas.NinjaModel;
 import jakarta.persistence.*;
-
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import java.util.List;
 
 @Entity
 @Table (name = "tb_missoes")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class MissoesModel {
 
     @Id
@@ -15,7 +19,6 @@ public class MissoesModel {
     private String nomeMissao;
     private String dificuldadeMissao;
 //    private int nivelMissao;
-
     // define that a mission has many ninjas
     @OneToMany(mappedBy = "missoes")
     private List<NinjaModel> ninjas;
--- src/main/java/dev/myproject/CadastroDeNinjas/Ninjas/NinjaModel.java
+++ src/main/java/dev/myproject/CadastroDeNinjas/Ninjas/NinjaModel.java
@@ -1,6 +1,9 @@
 package dev.myproject.CadastroDeNinjas.Ninjas;
 import dev.myproject.CadastroDeNinjas.Missoes.MissoesModel;
 import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
 /*
 * import Spring Data JPA and transform a class
@@ -8,51 +11,19 @@ import jakarta.persistence.*;
 * */
 @Entity
 @Table(name = "tb_cadastro")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class NinjaModel {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String nome;
     private String email;
     private int idade;
-
     // defines that the ninja has a single mission in the table
     @ManyToOne
     // connect NinjaModel with MissoesModel
     @JoinColumn(name = "missoes_id") // foreing key
     private MissoesModel missoes;
-
-    public NinjaModel() {
-    }
-
-    public NinjaModel(String nome, String email, int idade) {
-        this.nome = nome;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
